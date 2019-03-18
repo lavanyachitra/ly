90d5fd8d96a67db1b0fc6af35c6d15a82eb48158
lavanyachitra <lavanya19399@gmail.com>
2019-03-18 15:24:28 +0530

    Create prime.c

---
+++ prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,a,i;
+printf("enter the prime number");
+scanf("%d",&n);
+for(i=2;i<=n;i++)
+{
+a=2;
+while(i%a!=0)
+{
+i+=;
+}
+if(a==i)
+{
+printf("%d",i);
+}
+return 0;
+}
+}
